ff0270813fc81f97ca4e53ed3d5095795a0d7160
Rajprudvin <prudvirajn@gmail.com>
2024-08-31 15:14:30 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -77,7 +77,9 @@ VALIDATE $? "Schema loading"
 
 systemctl daemon-reload &>>$LOG_FILE
 VALIDATE $? "Daemon reload"
+
 systemctl enable backend &>>$LOG_FILE
 VALIDATE $? "Enabled backend"
+
 systemctl restart backend &>>$LOG_FILE
 VALIDATE $? "Restarted Backend"
