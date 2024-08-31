f80230471a00585a8a5e26402bc317a85d8fe17f
Rajprudvin <prudvirajn@gmail.com>
2024-08-31 16:51:27 +0530

    backend

--- frontend.sh
+++ frontend.sh
@@ -49,8 +49,6 @@ VALIDATE $? "Removing default website"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
 VALIDATE $? "Downloding frontend code"
- 
- mkdir -p /usr/share/nginx/html
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE
